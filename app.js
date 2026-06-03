8c16730feb6b3b9ed9e02cdc29c2a98aa89e45e1
RawatTushar <tusharrawatdpss@gmail.com>
2026-06-03 22:16:13 +0530

    new changes

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get('/secret', (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Basic ')) {
+    res.setHeader('WWW-Authenticate', 'Basic realm="Secret Area"');
+    return res.status(401).send('Authentication required');
+  }
+
+  const base64Credentials = authHeader.split(' ')[1];
+  const credentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+  const [username, password] = credentials.split(':');
+
+  if (
+    username === process.env.USERNAME &&
+    password === process.env.PASSWORD
+  ) {
+    return res.send(process.env.SECRET_MESSAGE);
+  }
+
+  res.setHeader('WWW-Authenticate', 'Basic realm="Secret Area"');
+  res.status(401).send('Invalid credentials');
+});
+
+const port = process.env.PORT 
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
