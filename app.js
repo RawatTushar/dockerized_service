0d4059e8940a5dc255e54b633c62489fdccbeec8
RawatTushar <tusharrawatdpss@gmail.com>
2026-06-03 23:04:01 +0530

    s

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, YARAA');
+  res.send('Hello, wow');
 });
 
 app.get('/secret', (req, res) => {
