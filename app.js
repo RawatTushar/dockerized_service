af653d00dd0b2abbcc8f32505c4194fe6e8b2e04
RawatTushar <tusharrawatdpss@gmail.com>
2026-06-03 22:28:19 +0530

    small change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Duniyaaa');
+  res.send('Hello, Sahabbb');
 });
 
 app.get('/secret', (req, res) => {
